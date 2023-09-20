2e7ed9142bb3a155539dbe7607ba808790d0634c
ItsSebis <sebi@sebis.net>
2023-09-20 22:21:16 +0200

    offer/buy system ready; server update loop prepared; working on daily update function

--- backend.js
+++ backend.js
@@ -2,6 +2,9 @@
 const fs = require('fs');
 const bcrypt = require('bcrypt');
 
+// update vars
+let lastDayCk = 0
+
 // webserver setup
 const express = require('express')
 const cookieParser = require('cookie-parser')
@@ -29,24 +32,43 @@ backend.get('/getcookie', (req, res) => {
 });
 
 // public vars
+let updating = false
 let users = {}
+let pubStats = {}
+
+// backend vars
 let passwords = {}
 const skel = {
     balance: 1000,
-    inventory: {
-        apple: 10,
-        apple_crate: 10
-    },
+    inventory: {},
+    offers: {},
+    special: "apple",
+    newSpecial: "apple",
 }
+const perishable = ["apple", "banana", "carrots"]
+const allItems = perishable + []
 const socketUser = {}
 const userSocket = {}
 
 // read data from file
-fs.readFile('./data.json', 'utf8', (err, data) => {
+fs.readFile('./data.json', 'utf8', (err, fileData) => {
     if (!err) {
-        users = JSON.parse(data).users
-        passwords = JSON.parse(data).passwords
+        const data = JSON.parse(fileData)
+        if (data.users !== undefined) {
+            users = data.users
+        }
+        if (data.stats !== undefined) {
+            pubStats = data.stats
+        }
+        if (data.passwords !== undefined) {
+            passwords = data.passwords
+        }
+        if (data.lastDayCk !== undefined) {
+            lastDayCk = data.lastDayCk
+        }
+        saveData()
         console.log("Loaded data!");
+        update().then()
     } else {
         throw err;
     }
@@ -60,6 +82,10 @@ io.on('connection', (socket) => {
 
     // auth
     socket.on('auth', (credentials) => {
+        if (updating) {
+            socket.emit('lError', "Update läuft, versuchen Sie es später erneut!")
+            return
+        }
         if (users[credentials.user] === undefined) {
             socket.emit('lError', "Account existiert nicht!")
             return
@@ -75,6 +101,7 @@ io.on('connection', (socket) => {
                 userSocket[credentials.user] = socket.id
                 socket.emit('login', credentials.user)
                 socket.emit('update', {users: users})
+                io.to('authenticated').emit('setOnline', Object.keys(userSocket))
                 console.log(credentials.user + " authenticated on " + socket.id)
             } else {
                 // user authentication failed
@@ -86,6 +113,10 @@ io.on('connection', (socket) => {
 
     // set password
     socket.on('pw', (data) => {
+        if (socketUser[socket.id] === null) {
+            socket.emit('kick')
+            return
+        }
         bcrypt.compare(data.old, passwords[socketUser[socket.id]], function(err, result) {
             if (result) {
                 // user authenticated
@@ -98,11 +129,135 @@ io.on('connection', (socket) => {
         });
     })
 
+    // reset user password
+    socket.on('resetPw', (user) => {
+        if (socketUser[socket.id] === null) {
+            socket.emit('kick')
+            return
+        }
+        if (user === "sebi" || users[user] === undefined || socketUser[socket.id] !== "sebi") {
+            return
+        }
+        const newPw = Math.round(Math.random() * (999999-100000) + 100000)
+        setPassword(user, newPw)
+        socket.emit('resetPwResult', "Passwort von '" + user + "' gesetzt auf `" + newPw + "`")
+    })
+
+    // create user request
+    socket.on('createUser', (username) => {
+        if (socketUser[socket.id] === null) {
+            socket.emit('kick')
+            return
+        }
+        if (username === "" || username === null || username === undefined || socketUser[socket.id] !== "sebi"
+            || users[username] !== undefined) {
+            return
+        }
+        users[username] = skel
+        users[username].username = username
+        const newPw = Math.round(Math.random() * (999999-100000) + 100000)
+        setPassword(username, newPw)
+        socket.emit('resetPwResult', "Benutzer '" + username + "' erstellt mit Passwort `" + newPw + "`")
+    })
+
+    // offer item by user
+    socket.on('offerItem', (offer) => {
+        if (socketUser[socket.id] === null) {
+            socket.emit('kick')
+            return
+        }
+        const seller = socketUser[socket.id]
+        const product = offer.item
+        const price = offer.price
+
+        if (users[seller].offers === undefined) {
+            users[seller].offers = {}
+        }
+        users[seller].offers[product] = price
+        saveData()
+    })
+    socket.on('removeOffer', (product) => {
+        if (socketUser[socket.id] === null) {
+            socket.emit('kick')
+            return
+        }
+        if (users[socketUser[socket.id]].offers[product] !== undefined) {
+            delete users[socketUser[socket.id]].offers[product]
+        }
+        saveData()
+    })
+    socket.on('buyOffer', (conditions) => {
+        if (socketUser[socket.id] === null) {
+            socket.emit('kick')
+            return
+        }
+        const buyer = socketUser[socket.id]
+        const seller = conditions.seller
+        const product = conditions.product
+        if (users[seller].offers === undefined || users[seller].offers[product] === undefined) {
+            socket.emit('cannotBuy', "Angebot existiert nicht mehr!")
+            return
+        }
+        const price = users[seller].offers[product]
+
+        if (users[buyer].balance < price) {
+            socket.emit('cannotBuy', "Nicht genug Geld!")
+            return
+        }
+        if (users[seller].inventory[product] === undefined || users[seller].inventory[product] <= 0) {
+            socket.emit('cannotBuy', "Ausverkauft!")
+            if (users[seller].inventory[product] !== undefined) {
+                delete users[buyer].inventory[product]
+            }
+            delete users[seller].offers[product]
+            return
+        }
+
+        if (users[buyer].inventory[product] === undefined) {
+            users[buyer].inventory[product] = 1
+        } else {
+            users[buyer].inventory[product] = users[buyer].inventory[product]+1
+        }
+
+        if (users[seller].inventory[product] <= 1) {
+            delete users[seller].inventory[product]
+        } else {
+            users[seller].inventory[product] = users[seller].inventory[product]-1
+        }
+
+        users[seller].balance += price
+        users[buyer].balance -= price
+
+        if (pubStats.items === undefined) {
+            pubStats.items = {}
+        }
+        if (pubStats.items[product] === undefined) {
+            pubStats.items[product] = {}
+        }
+        if (pubStats.items[product][price] === undefined) {
+            pubStats.items[product][price] = 0
+        }
+        pubStats.items[product][price]++
+
+        saveData()
+    })
+
+    // simulate update -> dev
+    socket.on('getUpdate', () => {
+        if (socketUser[socket.id] === null) {
+            socket.emit('kick')
+            return
+        }
+        io.to('authenticated').emit('update', {users: users})
+    })
+
     socket.on('disconnect', (reason) => {
+        console.log("Client disconnected: " + reason)
         if (userSocket[socketUser[socket.id]] === socket.id) {
             delete userSocket[socketUser[socket.id]]
         }
         delete socketUser[socket.id]
+        io.to('authenticated').emit('setOnline', Object.keys(userSocket))
     })
 })
 
@@ -110,12 +265,10 @@ io.on('connection', (socket) => {
 // set password for user
 function setPassword(user, password) {
     bcrypt.genSalt(10, function(err, salt) {
-        bcrypt.hash(password, salt, function(err, hash) {
+        bcrypt.hash(password.toString(), salt, function(err, hash) {
             // Store hash in your password DB.
             if (err) throw err;
             passwords[user] = hash
-            console.log(hash + " << " + password)
-            console.log(users)
             saveData()
         });
     });
@@ -126,7 +279,9 @@ function saveData() {
     io.to('authenticated').emit('update', {users: users})
     const data = {
         users: users,
+        stats: pubStats,
         passwords: passwords,
+        lastDayCk: lastDayCk,
     }
     fs.writeFile('./data.json', JSON.stringify(data), err => {
         if (err) {
@@ -140,3 +295,60 @@ function saveData() {
 server.listen(port, "0.0.0.0", () => {
     console.log(`Listening for connections on ${port}`)
 })
+
+async function update() {
+    const startTime = Date.now()
+    // update
+    const lastDayCkDate = new Date(lastDayCk)
+    const now = new Date(startTime)
+    if (
+        lastDayCkDate.getDate() !== now.getDate() ||
+        lastDayCkDate.getMonth() !== now.getMonth() ||
+        lastDayCkDate.getFullYear() !== now.getFullYear()
+    ) {
+        // new day
+        updating = true
+        io.emit('kick')
+        setTimeout(function () {
+            io.emit('kick')
+        }, 100)
+        for (const uid in users) {
+            // waste of intolerable goods
+            for (const iid in users[uid].inventory) {
+                if (perishable.includes(iid)) {
+                    let count = users[uid].inventory[iid]
+                    count = Math.round((Math.random() * (1/4) + 3/4) * count)
+                    users[uid].inventory[iid] = count
+                }
+                if (users[uid].inventory[iid] <= 0) {
+                    delete users[uid].inventory[iid]
+                }
+            }
+
+            if (users[uid].newSpecial !== undefined) {
+                users[uid].special = users[uid].newSpecial
+            }
+            // give new price
+            let avg = 5
+            users[uid].todayPrice = Math.round((Math.random() * (1/10*avg) + (avg - (1/20*avg))) * 100) / 100
+            users[uid].greatBuy = false
+        }
+        lastDayCk = startTime
+        saveData()
+        updating = false
+        console.log("New Day!")
+    }
+
+    const nextTime = now
+    nextTime.setMinutes(nextTime.getMinutes()+1)
+    nextTime.setSeconds(0)
+    nextTime.setMilliseconds(0)
+    let timeout = Date.parse(nextTime.toUTCString())-Date.now()
+    if (timeout <= 0) {
+        timeout = 0
+    }
+
+    setTimeout(function () {
+        update()
+    }, timeout)
+}
--- public/js/index.js
+++ public/js/index.js
@@ -4,10 +4,11 @@ const socket = io()
 // user data
 let me = ""
 let users = {}
+let online = []
 const itemNames = {
-    apple: "Apfel",
-    apple_crate: "Apfelkiste",
-    crate: "Kiste",
+    apple: "Apfelkiste",
+    banana: "Bananen",
+    carrots: "Karotten"
 }
 
 // login btn function
@@ -42,21 +43,149 @@ socket.on('pwChS', () => {
     document.getElementById('repPw').value = ""
 })
 
+// password reset result
+socket.on('resetPwResult', (res) => {
+    document.getElementById('resetRes').innerText = res
+})
+
+// cannot buy offer
+socket.on('cannotBuy', (err) => {
+    alert(err)
+})
+
 // update
 socket.on('update', (backendData) => {
     users = backendData.users
     document.getElementById('displayName').innerText = users[me].username
-    document.getElementById('balance').innerHTML = "<i class='bx bxs-dollar-circle' ></i> " + users[me].balance
+    document.getElementById('greatSpecial').innerText = itemNames[users[me].special]
+    document.getElementById('greatPrice').innerText = "$"+users[me].todayPrice+"/stk"
+    document.getElementById('balance').innerHTML = "<i class='bx bxs-dollar-circle' ></i> " + (Math.floor(users[me].balance*100)/100)
     for (const uid in users) {
-        const row = document.createElement("tr")
-        const nameCol = document.createElement("td")
-        const resetCol = document.createElement("td")
-        nameCol.innerText = uid
-        resetCol.innerHTML = "<i class='bx bx-reset' ></i>"
-        row.appendChild(nameCol)
-        row.appendChild(resetCol)
-        document.getElementById('usersBody').appendChild(row)
+        if (document.getElementById('manage'+uid) === null && uid !== "sebi") {
+            const row = document.createElement("tr")
+            const nameCol = document.createElement("td")
+            const resetCol = document.createElement("td")
+            row.id = "manage" + uid
+            row.classList.add("userRow")
+            row.setAttribute('data-user', uid)
+            nameCol.innerText = uid
+            resetCol.innerHTML = "<i class='bx bx-reset' ></i>"
+            resetCol.id = "reset" + uid
+            resetCol.classList.add("colBtn")
+            resetCol.onclick = function () {
+                socket.emit('resetPw', (uid))
+            }
+            row.appendChild(nameCol)
+            row.appendChild(resetCol)
+            document.getElementById('usersBody').appendChild(row)
+        }
     }
+    for (let i = 0; i < 3; i++) {
+        for (const uRow of document.getElementsByClassName("userRow")) {
+            if (users[uRow.getAttribute('data-user')] === undefined) {
+                uRow.remove()
+            }
+        }
+    }
+
+    for (const item in users[me].inventory) {
+        const count = users[me].inventory[item]
+        if (document.getElementById('show'+item) === null) {
+            const row = document.createElement("tr")
+            const nameCol = document.createElement("td")
+            const countCol = document.createElement("td")
+            const offerCol = document.createElement("td")
+            row.id = "show" + item
+            row.classList.add("itemRow")
+            row.setAttribute('data-item', item)
+            nameCol.innerText = itemNames[item]
+            countCol.innerText = count.toString()
+            offerCol.innerHTML = "<i class='bx bxs-store' ></i>"
+            offerCol.id = "offer" + me + item
+            offerCol.classList.add("colBtn")
+            offerCol.onclick = function () {
+                if (users[me].offers === undefined || users[me].offers[item] === undefined) {
+                    // set price
+                    let inputPrice = prompt("Was verlangen Sie für ihr Produkt '" + itemNames[item] + "'?")
+                    if (inputPrice === null) {
+                        return
+                    }
+                    while (!inputPrice.match(/^[+]?([0-9]*[.])?[0-9]+$/)) {
+                        inputPrice = prompt("Geben Sie eine Dezimalzahl als Preis ein z.B. '1.87'!")
+                    }
+                    const price = parseFloat(inputPrice)
+
+                    socket.emit('offerItem', ({price: price, item: item}))
+                } else {
+                    if (confirm("Möchten Sie ihr Angebot für " + itemNames[item] + " ($" + users[me].offers[item] + ") vom Markt nehmen?")) {
+                        socket.emit('removeOffer', (item))
+                        console.log("Offer removed")
+                    }
+                }
+            }
+            row.appendChild(nameCol)
+            row.appendChild(countCol)
+            row.appendChild(offerCol)
+            document.getElementById('itemsBody').appendChild(row)
+        } else {
+            document.getElementById('show'+item).getElementsByTagName('td')[1].innerText = count.toString()
+        }
+    }
+    for (let i = 0; i < 3; i++) {
+        for (const iRow of document.getElementsByClassName("itemRow")) {
+            if (users[me].inventory[iRow.getAttribute('data-item')] === undefined) {
+                iRow.remove()
+            } else {
+                const item = iRow.getAttribute('data-item')
+                iRow.getElementsByTagName('td')[1].innerText = users[me].inventory[item]
+            }
+        }
+    }
+
+    for (const uid in users) {
+        if (users[uid].offers === undefined || uid === me) {
+            continue
+        }
+        for (const tid in users[uid].offers) {
+            if (document.getElementById('trade' + uid + tid) === null) {
+                const row = document.createElement("tr")
+                const nameCol = document.createElement("td")
+                const priceCol = document.createElement("td")
+                const buyCol = document.createElement("td")
+                row.id = "trade" + uid + tid
+                row.classList.add("tradeRow")
+                row.setAttribute('data-trade-uid', uid)
+                row.setAttribute('data-trade-item', tid)
+                nameCol.innerHTML = itemNames[tid] + "<br>von " + uid
+                priceCol.innerText = "$" + users[uid].offers[tid].toString()
+                buyCol.innerHTML = "<i class='bx bx-target-lock'></i>"
+                buyCol.id = "request" + uid + tid
+                buyCol.classList.add("colBtn")
+                buyCol.onclick = function () {
+                    // request buy
+                    socket.emit('buyOffer', {seller: uid, product: tid})
+                }
+                row.appendChild(nameCol)
+                row.appendChild(priceCol)
+                row.appendChild(buyCol)
+                document.getElementById('tradeBody').appendChild(row)
+            }
+        }
+    }
+    for (let i = 0; i < 3; i++) {
+        for (const tRow of document.getElementsByClassName("tradeRow")) {
+            const seller = tRow.getAttribute('data-trade-uid')
+            const item = tRow.getAttribute('data-trade-item')
+            if (users[seller].offers[item] === undefined) {
+                tRow.remove()
+            }
+        }
+    }
+})
+
+// set online users
+socket.on('setOnline', (onlineUsers) => {
+    online = onlineUsers
 })
 
 // kick
@@ -94,3 +223,21 @@ document.getElementById('setPw').onclick = function () {
     }
     socket.emit('pw', {old: document.getElementById('oldPw').value, new: document.getElementById('newPw').value})
 }
+
+// great buy
+document.getElementById('greatBuyBtn').onclick = function () {
+
+}
+
+// create user
+function createUser() {
+    const username = prompt("New username...")
+
+    if (username === "" || username === null || username === undefined) {
+        return
+    }
+    socket.emit('createUser', username)
+}
+
+// focus on login
+document.getElementById("username").focus();
